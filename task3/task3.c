d05129e10fc344d270b32e1a8a402538099a9248
skoryknataliia <skoryk.nataliia1622@gmail.com>
2024-11-17 11:50:44 +0200

    Оновлено task3

--- task3/task3.c
+++ task3/task3.c
@@ -5,14 +5,13 @@
 
 int main() {
 
-//створюємо масив із словами
     char *WORDS[] = { "day", "night", "year" };
     int count = sizeof(WORDS) / sizeof(WORDS[0]);
 
-    // ініціалізуємо генератор випадкового числа
+
     srand(time(NULL));
 
-    
+
     int randomIndex = rand() % count;
     char *selectedWord = WORDS[randomIndex];
     int wordLength = strlen(selectedWord);
@@ -22,6 +21,7 @@ int main() {
     for (int i = 0; i < wordLength; i++) {
         hideWord[i] = '*';
     }
+    hideWord[wordLength] = '\0'; 
 
 
     int attempts = 3;
@@ -30,11 +30,19 @@ int main() {
 
 
     while (attempts > 0) {
+        printf("Hidden word: %s\n", hideWord);
         printf("Input letter: ");
         scanf(" %c", &letter);
-        
-        printf("Input position: ");
+
+        printf("Input position (0 to %d): ", wordLength - 1);
         scanf("%d", &position);
+
+
+        if (position < 0 || position >= wordLength) {
+            printf("Invalid position! Try again.\n");
+            continue;
+        }
+
 
         if (selectedWord[position] == letter) {
             hideWord[position] = letter;
@@ -44,12 +52,11 @@ int main() {
             printf("Incorrect! Attempts left: %d\n", attempts);
         }
 
-        printf("Current word: %s\n", hideWord);
-
         if (strcmp(selectedWord, hideWord) == 0) {
             printf("Congratulations, you guessed the word: %s\n", selectedWord);
             break;
         }
+
 
         if (attempts == 0) {
             printf("Sorry, you lost. The word was: %s\n", selectedWord);
